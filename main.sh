fe4c3bca844bb80d910921247051f621c0e45f41
SoulOfHope <azzynchara@outlook.com>
2023-02-16 15:33:22 +0000

    updated

--- main.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd Breaker-Official-Release-v1.3.8
-python3 main.py
--- shell.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-result=subprocess.run(["python3","-m","pip","install","sly"])
-print(result.stdout)
